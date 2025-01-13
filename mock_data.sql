d8ddae397a442a17cc9cdd3098c4afcd5ca05644
s3alard <laszlo.szilard2000@gmail.com>
2025-01-13 12:31:17 +0100

    fix mock data insertion order

--- mock_data.sql
+++ mock_data.sql
@@ -1,5 +1,103 @@
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Great Sword'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Long Sword'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Sword and Shield'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Dual Blades'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Lance'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Gunlance'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Hammer'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Hunting Horn'
+  ,'Melee');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Bow'
+  ,'Ranged');
+INSERT INTO hunter_weapons
+  (NAME
+  ,TYPE)
+VALUES
+  ('Heavy Bowgun'
+  ,'Ranged');
+
+INSERT INTO hunter_armor
+  (NAME
+  ,monster_id)
+VALUES
+  ('Banbaro Armor'
+  ,100);
+INSERT INTO hunter_armor
+  (NAME
+  ,monster_id)
+VALUES
+  ('Beotodus Armor'
+  ,101);
+INSERT INTO hunter_armor
+  (NAME
+  ,monster_id)
+VALUES
+  ('Nargacuga Armor'
+  ,102);
+INSERT INTO hunter_armor
+  (NAME
+  ,monster_id)
+VALUES
+  ('Velkhana Armor'
+  ,103);
+INSERT INTO hunter_armor
+  (NAME
+  ,monster_id)
+VALUES
+  ('Tigrex Armor'
+  ,104);
+INSERT INTO hunter_armor
+  (NAME
+  ,monster_id)
+VALUES
+  ('Legiana Armor'
+  ,105);
+
 INSERT INTO monsters
-  (name
+  (NAME
   ,TYPE
   ,weakness)
 VALUES
@@ -7,7 +105,7 @@ VALUES
   ,'Brute Wyvern'
   ,'Dragon');
 INSERT INTO monsters
-  (name
+  (NAME
   ,TYPE
   ,weakness)
 VALUES
@@ -15,7 +113,7 @@ VALUES
   ,'Piscine Wyvern'
   ,'Fire');
 INSERT INTO monsters
-  (name
+  (NAME
   ,TYPE
   ,weakness)
 VALUES
@@ -23,7 +121,7 @@ VALUES
   ,'Flying Wyvern'
   ,'Thunder');
 INSERT INTO monsters
-  (name
+  (NAME
   ,TYPE
   ,weakness)
 VALUES
@@ -31,7 +129,7 @@ VALUES
   ,'Elder Dragon'
   ,'Fire');
 INSERT INTO monsters
-  (name
+  (NAME
   ,TYPE
   ,weakness)
 VALUES
@@ -39,7 +137,7 @@ VALUES
   ,'Brute Wyvern'
   ,'Thunder');
 INSERT INTO monsters
-  (name
+  (NAME
   ,TYPE
   ,weakness)
 VALUES
@@ -48,142 +146,44 @@ VALUES
   ,'Fire');
 
 INSERT INTO areas
-  (name
+  (NAME
   ,monster_id)
 VALUES
   ('Ancient Forest'
   ,103);
 INSERT INTO areas
-  (name
+  (NAME
   ,monster_id)
 VALUES
   ('Wildspire Waste'
   ,101);
 INSERT INTO areas
-  (name
+  (NAME
   ,monster_id)
 VALUES
   ('Coral Highlands'
   ,102);
 INSERT INTO areas
-  (name
+  (NAME
   ,monster_id)
 VALUES
   ('Hoarfrost Reach'
   ,103);
 INSERT INTO areas
-  (name
+  (NAME
   ,monster_id)
 VALUES
   ('Elder’s Recess'
   ,104);
 INSERT INTO areas
-  (name
+  (NAME
   ,monster_id)
 VALUES
   ('Rotten Vale'
   ,100);
 
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Great Sword'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Long Sword'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Sword and Shield'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Dual Blades'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Lance'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Gunlance'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Hammer'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Hunting Horn'
-  ,'Melee');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Bow'
-  ,'Ranged');
-INSERT INTO hunter_weapons
-  (name
-  ,TYPE)
-VALUES
-  ('Heavy Bowgun'
-  ,'Ranged');
-
-INSERT INTO hunter_armor
-  (name
-  ,monster_id)
-VALUES
-  ('Banbaro Armor'
-  ,100);
-INSERT INTO hunter_armor
-  (name
-  ,monster_id)
-VALUES
-  ('Beotodus Armor'
-  ,101);
-INSERT INTO hunter_armor
-  (name
-  ,monster_id)
-VALUES
-  ('Nargacuga Armor'
-  ,102);
-INSERT INTO hunter_armor
-  (name
-  ,monster_id)
-VALUES
-  ('Velkhana Armor'
-  ,103);
-INSERT INTO hunter_armor
-  (name
-  ,monster_id)
-VALUES
-  ('Tigrex Armor'
-  ,104);
-INSERT INTO hunter_armor
-  (name
-  ,monster_id)
-VALUES
-  ('Legiana Armor'
-  ,105);
-
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -193,7 +193,7 @@ VALUES
   ,2000
   ,5);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -203,7 +203,7 @@ VALUES
   ,2001
   ,3);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -213,7 +213,7 @@ VALUES
   ,2002
   ,7);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -223,7 +223,7 @@ VALUES
   ,2003
   ,2);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -233,7 +233,7 @@ VALUES
   ,2004
   ,9);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -243,7 +243,7 @@ VALUES
   ,2005
   ,1);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -253,7 +253,7 @@ VALUES
   ,2002
   ,4);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -263,7 +263,7 @@ VALUES
   ,2003
   ,6);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -273,7 +273,7 @@ VALUES
   ,2004
   ,8);
 INSERT INTO hunters
-  (name
+  (NAME
   ,weapon_id
   ,armor_id
   ,monsters_hunted)
@@ -282,5 +282,3 @@ VALUES
   ,1009
   ,2005
   ,0);
-
-SELECT * FROM hunters;
