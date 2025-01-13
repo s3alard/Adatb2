caa534f3750b0089c257f9379d15e18f791e793e
s3alard <laszlo.szilard2000@gmail.com>
2025-01-13 10:56:19 +0100

    fixed triggers

--- triggers.sql
+++ triggers.sql
@@ -12,8 +12,8 @@ CREATE OR REPLACE TRIGGER monsters_id_trg
 BEFORE INSERT ON monsters
   FOR EACH ROW
 BEGIN
-  IF :new.id IS NULL THEN
-     :new.id := monster_id_seq.nextval;
+  IF :new.monster_id IS NULL THEN
+     :new.monster_id := monster_id_seq.nextval;
   END IF;
 END monsters_id_trg;
 /
@@ -22,8 +22,8 @@ CREATE OR REPLACE TRIGGER areas_id_trg
 BEFORE INSERT ON areas
   FOR EACH ROW
 BEGIN
-  IF :new.id IS NULL THEN
-     :new.id := area_id_seq.nextval;
+  IF :new.area_id IS NULL THEN
+     :new.area_id := area_id_seq.nextval;
   END IF;
 END maps_id_trg;
 /
@@ -32,8 +32,8 @@ CREATE OR REPLACE TRIGGER weapons_id_trg
 BEFORE INSERT ON hunter_weapons
   FOR EACH ROW
 BEGIN
-  IF :new.id IS NULL THEN
-     :new.id := weapon_id_seq.nextval;
+  IF :new.weapon_id IS NULL THEN
+     :new.weapon_id := weapon_id_seq.nextval;
   END IF;
 END weapons_id_trg;
 /
@@ -42,8 +42,8 @@ CREATE OR REPLACE TRIGGER armor_id_trg
 BEFORE INSERT ON hunter_armor
   FOR EACH ROW
 BEGIN
-  IF :new.id IS NULL THEN
-     :new.id := armor_id_seq.nextval;
+  IF :new.armor_id IS NULL THEN
+     :new.armor_id := armor_id_seq.nextval;
   END IF;
 END armor_id_trg;
 /
@@ -52,8 +52,8 @@ CREATE OR REPLACE TRIGGER hunters_id_trg
 BEFORE INSERT ON hunters
   FOR EACH ROW
 BEGIN
-  IF :new.id IS NULL THEN
-     :new.id := hunter_id_seq.nextval;
+  IF :new.hunter_id IS NULL THEN
+     :new.hunter_id := hunter_id_seq.nextval;
   END IF;
 END hunters_id_trg;
 /
